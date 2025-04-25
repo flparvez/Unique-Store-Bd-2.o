40e1b5101fd8828b3f173f9544c06ce94b5e47ec
Md Parvez <flparvez23@gmail.com>
2025-04-25 20:36:31 +0600

    try fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
 import { apiClient } from "@/lib/api-client";
+import { Loader2 } from "lucide-react";
 import Link from "next/link";
 
 
@@ -10,7 +11,14 @@ export default async function Home() {
 
           const response = await apiClient.getProducts();
 
-      
+      if (!response) {
+        // skeleton
+        return (
+          <div className="flex items-center justify-center h-64">
+            <Loader2 className="h-8 w-8 animate-spin" />
+          </div>
+        );
+      }
   return (
     <div>
       <Link className="text-xl " href="/admin/product/create-product">Create Product</Link>

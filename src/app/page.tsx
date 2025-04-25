2783b161cd09e2b274e6e007ede1b48b11def156
Md Parvez <flparvez23@gmail.com>
2025-04-25 21:07:21 +0600

    test 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,23 @@
 
 
 import CategoryList from "@/components/CategoryList";
-// import ProductList from "@/components/ProductList";
-// import { apiClient } from "@/lib/api-client";
-// import { Loader2 } from "lucide-react";
+import ProductList from "@/components/ProductList";
+import { apiClient } from "@/lib/api-client";
+import { ApiResponse } from "@/types/product";
+
 import Link from "next/link";
 
 
 export default async function Home() {
 
-      //     const response = await apiClient.getProducts();
-
-      // if (!response) {
-      //   // skeleton
-      //   return (
-      //     <div className="flex items-center justify-center h-64">
-      //       <Loader2 className="h-8 w-8 animate-spin" />
-      //     </div>
-      //   );
-      // }
+  const productsData: ApiResponse = await apiClient.getProducts();
+  
+  
   return (
     <div>
       <Link className="text-xl " href="/admin/product/create-product">Create Product</Link>
       <h2 className="text-2xl text-red-700">Next Js Landing Page</h2>
-   {/* <ProductList product={response} /> */}
+   <ProductList product={productsData} />
 
       <h2>Category</h2>
 
